2f2c1042e9916d72972c1d899a03d0e24dde3c0b
gowtham123-pro <gowthammdb7@gmail.com>
2021-07-23 15:17:06 +0530

    new ui

--- mongo_boy/mongoose.js
+++ mongo_boy/mongoose.js
@@ -1,5 +1,5 @@
 const mg=require('mongoose')
 
-mg.connect('mongodb+srv://sai:saisai99122@cluster0.0fn1n.mongodb.net/newdb.log?retryWrites=true&w=majority',{useCreateIndex:true,useFindAndModify:false,useNewUrlParser:true,useUnifiedTopology:true})
+mg.connect('mongodb+srv://sai:saisai99122@cluster0.0fn1n.mongodb.net/CRUD?retryWrites=true&w=majority',{useCreateIndex:true,useFindAndModify:false,useNewUrlParser:true,useUnifiedTopology:true})
 
 
